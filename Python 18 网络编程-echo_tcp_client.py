01f795575cd38e7dc7b3fcb35b2deffabea4fc1f
qin2 <qin2@qq.com>
2015-11-29 17:18:50 +0800

    add echo_tcp_server and client

---
+++ Python 18 网络编程-echo_tcp_client.py
@@ -0,0 +1,23 @@
+import socket
+import sys
+
+#HOST, PORT = "localhost", 5551
+HOST, PORT = "qinuu.com", 5551
+data = " ".join(sys.argv[1:])
+data = "Hello!"
+
+# Create a socket (SOCK_STREAM means a TCP socket)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    # Connect to server and send data
+    sock.connect((HOST, PORT))
+    sock.sendall(bytes(data + "\n", "utf-8"))
+
+    # Receive data from the server and shut down
+    received = str(sock.recv(1024), "utf-8")
+finally:
+    sock.close()
+
+print("Sent:     {}".format(data))
+print("Received: {}".format(received))
